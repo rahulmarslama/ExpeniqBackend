4d17e86676efdd10ab5682d44f28d186f99edc87
rahulmarslama <rahulrlama@miu.edu>
2026-03-26 10:15:30 -0500

    API Gateway configured with Ocelot

---
+++ APIGateway/Program.cs
@@ -0,0 +1,33 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Configuration.AddJsonFile("GatewayConfiguration.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseOcelot().Wait();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
